965c376eefdf9f7e56acea8253934a2b4cbab623
Rishan <98330109+NadimMahmudRishan@users.noreply.github.com>
2024-11-01 11:54:33 +0600

    some view page created and connect with route.

---
+++ resources/views/about.blade.php
@@ -0,0 +1,4 @@
+<h1>About</h1>
+<h2>{{$name}}</h2>
+<a href="/">Welcome</a>
+<a href="/home">Home</a>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/home', function () {
+    return view('home');
 });
+
+Route::get('/about/{name}',function($name){
+    echo $name;
+    return view('about',['name'=>$name]);
+});
+
+Route::redirect('/','/home');
+// Another way ot declear route.
+// Route::view("/home","home");
